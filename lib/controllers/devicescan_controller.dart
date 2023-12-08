f0472aaa5e911a9cd2260fe919e3d8f1d405c2a5
gmathu <mathu.ke@gmail.com>
2023-12-08 12:07:36 +0300

    added plugin & instance

--- lib/controllers/devicescan_controller.dart
+++ lib/controllers/devicescan_controller.dart
@@ -1,3 +1,15 @@
-class DeviceController {
+import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 
+class DeviceController {
+  //bluetooth instance
+  BluetoothManager bluetoothManager = BluetoothManager.instance;
+
+  //scanning for devices
+  Future <Stream> scanForDevices() async{
+    bluetoothManager.startScan()
+        .then((value) => null);
+    final result =
+        bluetoothManager.scanResults;
+    return result;
+  }
 }
