2869ba146fe6a6c1e9d0bd259cae45bd00488d57
gmathu <mathu.ke@gmail.com>
2023-12-08 11:09:27 +0300

    feature: new class and controller. and extending the controller

---
+++ lib/pages/devicescaning_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:musicapp/controllers/devicescan_controller.dart';
+
+class DeviceScanningPage extends GetView<DeviceController>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
